41730d83c65b97e204c50e079a27e57b05a18950
Reetesh Kesarwani <114504936+reeteshk@users.noreply.github.com>
2024-02-09 12:00:44 -0500

    Update InsertIntoBST.java updating the recursive solution

--- BinarySearchTree/InsertIntoBST.java
+++ BinarySearchTree/InsertIntoBST.java
@@ -43,4 +43,17 @@ class Solution {
         }
         return cur;
     }
+
+    //recursive approach
+    public TreeNode insertIntoBST(TreeNode root, int val) {
+        if (root == null) {
+            return new TreeNode(val);
+        }
+        if (val > root.val) {
+            root.right = insertIntoBST(root.right, val);
+        } else {
+            root.left = insertIntoBST(root.left, val);
+        }
+        return root;
+    }
 }
